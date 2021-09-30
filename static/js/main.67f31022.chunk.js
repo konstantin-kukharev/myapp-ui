4dd9cdf83faca3a896a8700400fd82c255be333f
konstantin-kukharev <68326190+konstantin-kukharev@users.noreply.github.com>
2021-09-30 16:49:30 +0000

    Deploying to build/pages from  @ 728577657f3bd41dc5fc5e48d9fee994dcee1c6e 🚀

--- static/js/main.67f31022.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonptemplate-admin-app"]=this["webpackJsonptemplate-admin-app"]||[]).push([[0],{9:function(e,t,p){"use strict";p.r(t);p(0);var a=p(3),n=p.n(a),c=p(1);var i=()=>Object(c.jsx)("h1",{children:"Application"});n.a.render(Object(c.jsx)(i,{}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.67f31022.chunk.js.map
